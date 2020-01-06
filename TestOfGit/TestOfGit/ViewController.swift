231489fa14ea272ca536dc89726fa99a6abcac10
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-01-06 23:39:58 +0900

    first commit

---
+++ TestOfGit/TestOfGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestOfGit
+//
+//  Created by Tatsuya Yamamoto on 2020/1/6.
+//  Copyright © 2020 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
